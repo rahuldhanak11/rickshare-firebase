e72254f882c1e835b0a42c7ffaee5e97afd3046d
aryagami26 <94133032+aryagami26@users.noreply.github.com>
2024-04-01 23:14:31 +0530

    Update Firebase initialization in main.dart
    Change from:
    ```dart
    await Firebase.initializeApp();
    ```
    to:
    ```dart
    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
    ```
    Add import for `firebase_options.dart`.

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,11 @@ import 'package:rickshare/Views/Splashscrn.dart';
 import 'package:rickshare/Views/Welcomescrn.dart';
 import 'package:rickshare/Views/Signupscrn.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:rickshare/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
